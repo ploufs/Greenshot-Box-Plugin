974201f4404c74fc96ac4e7d4443402d0f591ecb
Francis Noel <ploufs@hotmail.com>
2011-12-31 11:28:50 -0500

    Add bath file (install plugin)
    Use : SendToGreenShot <GreenShot path>

---
+++ GreenshotBoxPlugin/SendToGreenShot.bat
@@ -0,0 +1,12 @@
+﻿REM copy file to GreenShot folder
+@echo off
+cls
+
+set GreenShotPath=%1
+set PlugInPath="\Plugins\GreenshotBoxPlugin"
+
+MD %GreenShotPath%%PlugInPath%
+copy "bin\Release\GreenshotBoxPlugin.gsp" %GreenShotPath%%PlugInPath%\GreenshotBoxPlugin.gsp
+
+MD %GreenShotPath%\Languages\%PlugInPath%
+copy "Languages\*.*" %GreenShotPath%\Languages\%PlugInPath%
